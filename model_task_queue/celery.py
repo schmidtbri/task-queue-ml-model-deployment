1fcf8df09fd11c69f2fc4cff5e5a75b83d3c7bf7
Brian Schmidt <brian@Brians-MacBook-Pro.local>
2019-10-15 20:16:30 -0400

    fixing config

--- model_task_queue/celery.py
+++ model_task_queue/celery.py
@@ -5,22 +5,20 @@ from celery.app.registry import TaskRegistry
 
 from model_task_queue import __name__
 from model_task_queue.ml_model_task import MLModelPredictionTask
-import model_task_queue.config as config
+from model_task_queue.config import Config
 
 
 # creating a TaskRegistry in order to be able to instantiate a dynamic number of task in the celery app
 registry = TaskRegistry()
 
 # instantiating the MLModelPredictionTask objects and adding them to a TaskRegistry object
-for model in config.models:
+for model in Config.models:
     registry.register(MLModelPredictionTask(module_name=model["module_name"],
                                             class_name=model["class_name"]))
 
 # instantiating the Celery app object
 app = Celery(__name__,
-             broker='redis://localhost:6379/0',
-             backend='redis://localhost:6379/0',
              tasks=registry)
 
 # importing the connection settings
-app.config_from_object("model_task_queue.config.{}".format(os.environ['APP_SETTINGS']))
+app.config_from_object("model_task_queue.config:{}".format(os.environ['APP_SETTINGS']))
--- model_task_queue/config.py
+++ model_task_queue/config.py
@@ -1,42 +1,40 @@
 """Configuration settings for the celery application."""
 
-models = [
-    {
-        "module_name": "iris_model.iris_predict",
-        "class_name": "IrisModel"
-    }
-]
-
 
 class Config(object):
     """Configuration for all environments."""
 
-    pass
+    models = [
+        {
+            "module_name": "iris_model.iris_predict",
+            "class_name": "IrisModel"
+        }
+    ]
 
 
 class ProdConfig(Config):
     """Configuration for the prod environment."""
 
-    backend = 'redis://localhost:6379/0',
-    broker = 'redis://localhost:6379/0'
+    broker_url = 'redis://localhost:6379/0'
+    result_backend = 'redis://localhost:6379/0'
 
 
 class BetaConfig(Config):
     """Configuration for the beta environment."""
 
-    backend = 'redis://localhost:6379/0',
-    broker = 'redis://localhost:6379/0'
+    broker_url = 'redis://localhost:6379/0'
+    result_backend = 'redis://localhost:6379/0'
 
 
 class TestConfig(Config):
     """Configuration for the test environment."""
 
-    backend = 'redis://localhost:6379/0',
-    broker = 'redis://localhost:6379/0'
+    broker_url = 'redis://localhost:6379/0'
+    result_backend = 'redis://localhost:6379/0'
 
 
 class DevConfig(Config):
     """Configuration for the dev environment."""
 
-    backend = 'redis://localhost:6379/0',
-    broker = 'redis://localhost:6379/0'
+    broker_url = 'redis://localhost:6379/0'
+    result_backend = 'redis://localhost:6379/0'
